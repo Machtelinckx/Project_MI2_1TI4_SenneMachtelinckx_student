c5282d60e24b5b30ea3da1113d58abe02e5e95f6
Machtelinckx <senne.machtelinckx@student.odisee.be>
2016-05-21 10:26:16 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,112 +1,134 @@
 "use strict";
-var kaartenHart = ["img/clubs2.png",
-                    "img/clubs3.png",
-                    "img/clubs4.png",
-                           "img/clubs5.png", 
-                           "img/clubs6.png",
-                           "img/clubs7.png",
-                           "img/clubs8.png",
-                           "img/clubs9.png",
-                           "img/clubs10.png",
-                           "img/clubsj.png",
-                           "img/clubsq.png",
-                           "img/clubsk.png",
-                           "img/clubsa.png" ];
+var kaarten = ["img/clubs2.png",
+                   "img/clubs3.png",
+                   "img/clubs4.png",
+                   "img/clubs5.png", 
+                   "img/clubs6.png",
+                   "img/clubs7.png",
+                   "img/clubs8.png",
+                   "img/clubs9.png",
+                   "img/clubs10.png",
+                   "img/clubsj.png",
+                   "img/clubsq.png",
+                   "img/clubsk.png",
+                   "img/clubsa.png" ];
 
         
-        var randomNummer = Math.floor((Math.random() * kaartenHart.length));
+        var randomNummer = Math.floor((Math.random() * kaarten.length));
         var punten = 0;
         var aantalPogingen = 5;
         
         function drawKaart() {
-            $("#afbeeldingKaart").attr("src", kaartenHart[randomNummer]);
+            $("#afbeeldingKaart").attr("src", kaarten[randomNummer]);
         }
 
         $(document).ready(drawKaart);
         
        function hogereKaart() {
            if (aantalPogingen > 0) {
-           var hoger = Math.floor((Math.random() * kaartenHart.length)); 
-           $("#afbeeldingKaart").attr("src", kaartenHart[hoger]); 
+           var hoger = Math.floor((Math.random() * kaarten.length)); 
+           $("#afbeeldingKaart").attr("src", kaarten[hoger]); 
            if (hoger >= randomNummer){
                punten++; 
                $("#score2").html("CORRECT +1");
-               $("#pogingen").html("SCORE : " + punten);
                
-               $("#score2").fadeOut(1500, function(){
+               $("#score2").fadeOut(800, function(){
                    $("#score2").fadeIn(100);
                    $("#score2").html("SCORE : " + punten);
                 });
-               $("#pogingen").fadeOut(1500, function(){
-                   $("#pogingen").fadeIn(100);
-                   $("#pogingen").html("Wrong guesses left : " + aantalPogingen);
-               });
             }
             else 
             {
                 aantalPogingen--; 
-                if(aantalPogingen == 0)
+                $("#score2").html("FAULT -1");
+               
+                $("#score2").fadeOut(800, function(){
+                   $("#score2").fadeIn(100);
+                   $("#score2").html("SCORE : " + punten);
+                });
+                
+                if(aantalPogingen == 4)
                 {
-                    $("#pogingen").html("Wrong guesses left : " + aantalPogingen);
+                    $("#hart5").attr("src", "img/hart1.png");
+                    
+                }
+                else if(aantalPogingen == 3)
+                {
+                    $("#hart4").attr("src", "img/hart1.png");
+                    
+                }
+                else if(aantalPogingen == 2)
+                {
+                    $("#hart3").attr("src", "img/hart1.png");
+                    
+                }
+                else if(aantalPogingen == 1)
+                {
+                    $("#hart2").attr("src", "img/hart1.png");
+                    
+                }
+                else if (aantalPogingen == 0)
+                {
                     gameOver();
                 }
-                else
-                {
-                    $("#score2").html("FAULT -1");
-                    $("#pogingen").html("Wrong guesses left : " + aantalPogingen);
-               
-                    $("#score2").fadeOut(1500, function(){
-                        $("#score2").fadeIn(100);
-                        $("#score2").html("SCORE : " + punten);
-                    });
-                }   
             }
            
              randomNummer = hoger;
-             $("#afbeeldingKaart").attr("src", kaartenHart[hoger]);
+             $("#afbeeldingKaart").attr("src", kaarten[hoger]);
                }
        }
         
 function lagereKaart() {
     if (aantalPogingen > 0) {
-        var lager = Math.floor((Math.random() * kaartenHart.length));
-        $("#afbeeldingKaart").attr("src", kaartenHart[lager]); 
+        var lager = Math.floor((Math.random() * kaarten.length));
+        $("#afbeeldingKaart").attr("src", kaarten[lager]); 
         if (lager <= randomNummer){
             punten++; 
             $("#score2").html("CORRECT +1");
-            $("#pogingen").html("SCORE : " + punten);
             
-            $("#score2").fadeOut(1500, function(){
+            $("#score2").fadeOut(800, function(){
                 $("#score2").fadeIn(100);
                 $("#score2").html("SCORE : " + punten);
-            });
-            $("#pogingen").fadeOut(1500, function(){
-                $("#pogingen").fadeIn(100);
-                $("#pogingen").html("Wrong guesses left : " + aantalPogingen);
             });
         }
         
         else
         {
-            aantalPogingen--;
-            if(aantalPogingen == 0) 
-            {
-                gameOver();
-            }
-            else
-            {
+            aantalPogingen--; 
                 $("#score2").html("FAULT -1");
-                $("#pogingen").html("Wrong guesses left : " + aantalPogingen);
                
-                $("#score2").fadeOut(1500, function(){
-                    $("#score2").fadeIn(100);
-                    $("#score2").html("SCORE : " + punten);
+                $("#score2").fadeOut(800, function(){
+                   $("#score2").fadeIn(100);
+                   $("#score2").html("SCORE : " + punten);
                 });
-               
-            }   
+                
+                if(aantalPogingen == 4)
+                {
+                    $("#hart5").attr("src", "img/hart1.png");
+                    
+                }
+                else if(aantalPogingen == 3)
+                {
+                    $("#hart4").attr("src", "img/hart1.png");
+                    
+                }
+                else if(aantalPogingen == 2)
+                {
+                    $("#hart3").attr("src", "img/hart1.png");
+                    
+                }
+                else if(aantalPogingen == 1)
+                {
+                    $("#hart2").attr("src", "img/hart1.png");
+                    
+                }
+                else if (aantalPogingen == 0)
+                {
+                    gameOver();
+                }
         }
         randomNummer = lager;
-        $("#afbeeldingKaart").attr("src", kaartenHart[lager]);
+        $("#afbeeldingKaart").attr("src", kaarten[lager]);
     }
 }
         
@@ -133,28 +155,13 @@ function playAgain() {
     $("#pogingen").html("Wrong guesses left : 5 ");
 }
 
+// DATABASE VOOR TOPSCORE AANMAKEN
 
-var highScore, teller, tText, HigherLowerDatabase3, db, naam, score;
-
-function updateTablesSQL() {
-				HigherLowerDatabase3.transaction(function (tx) {
-					tx.executeSql('SELECT * FROM highscores order by score desc', [], function (tx, results) {
-						var len = 1;
-						tText = "";
-						for (teller = 0; teller < len; teller += 1) {
-							tText += "<tr><td>" + results.rows.item(teller).score + "<\/td><\/tr>";
-						}
-						$("#highscoreTableDB tbody").html(tText);
-                        $("#topscoorder").html("Highscore : " + tText);
-
-					}, null);
-				});
-
-			}
+var topscore, rijen, topscoreTekst, HigherLowerDatabase, db, naam, score;
 
 $(document).ready(function () {
     score = 0;
-    highScore = [{
+    topscore = [{
         "score" : score
     }];
     
@@ -162,40 +169,57 @@ $(document).ready(function () {
 					$.mobile.defaultPageTransition = 'none';
 				});
     
-    HigherLowerDatabase3 = openDatabase('hl3Database', '1.0', 'DB', 1 * 1024 * 1024);
+    HigherLowerDatabase = openDatabase('hlDatabase', '1.0', 'DB', 1 * 1024 * 1024);
 
-HigherLowerDatabase3.transaction(function (tx) {
+HigherLowerDatabase.transaction(function (tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS highscores (score)');
-					tx.executeSql('select count(*) as aantal from highscores where score like ?', [score], function (tx, results) {
-						console.log("select werkt");
-						if (results.rows.item(0).aantal === 0) {
-							tx.executeSql('INSERT INTO highscores (score) VALUES (?)', [score], function (tx, results) {
-								console.log("ok!");
-							}, function (tx, error) {
-								console.log("NOK!");
-							}); 
-						}
+    tx.executeSql('select count(*) as aantal from highscores where score like ?', [score], function (tx, results) {
+        console.log("select werkt");
+        if (results.rows.item(0).aantal === 0) {
+            tx.executeSql('INSERT INTO highscores (score) VALUES (?)', [score], function (tx, results) {
+                console.log("ok!");
+            }, function (tx, error) {
+                console.log("NOK!");
+            }); 
+        }
 
-					}, function (tx, error) {
-						console.log("NOK!");
-					});
-				});
+    }, function (tx, error) {
+        console.log("NOK!");
+    });
+});
 
 				
-				updateTablesSQL();
+    updateTablesSQL();
 
-				$("#voegToe").click(function () {
-					score = punten;
-					highScore.push({
-						"score" : score
-					});
+    $("#voegToe").click(function () {
+        score = punten;
+        topscore.push({
+            "score" : score
+        });
 
-					HigherLowerDatabase3.transaction(function (tx) {
-						tx.executeSql('INSERT INTO highscores (score) VALUES (?)', [score]);
-					});
+        HigherLowerDatabase.transaction(function (tx) {
+            tx.executeSql('INSERT INTO highscores (score) VALUES (?)', [score]);
+        });
 					
-					updateTablesSQL();
+        updateTablesSQL();
 
-				});
+    });
 
-			});
+});
+
+
+function updateTablesSQL() {
+    HigherLowerDatabase.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM highscores order by score desc', [], function (tx, results) {
+            var lengte = 1;
+            topscoreTekst = "";
+            for (rijen = 0; rijen < lengte; rijen += 1) {
+                topscoreTekst += "<tr><td>" + results.rows.item(rijen).score + "<\/td><\/tr>";
+            }
+            $("#highscoreTableDB tbody").html(topscoreTekst);
+            $("#topscoorder").html(topscoreTekst);
+
+        }, null);
+    });
+
+}
